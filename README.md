dc73a244692bfcc0165754e1cc09455f97643f4f
Yon@than <jodex2902@gmail.com>
2023-12-31 16:00:10 +0300

    compeleted full feature and updated readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# MERN Book Store Manager
+
+This is a simple Book Store Manager application built using the MERN (MongoDB, Express.js, React, Node.js) stack. It allows users to manage books by providing features like adding, editing, and deleting books.
+
+## Features
+
+* View Books: See a list of all books with details such as title, author, and published year.
+* Add Book: Add a new book to the store.
+* Edit Book: Modify the details of an existing book.
+* Delete Book: Remove a book from the store.
+
+## Technologies Used
+
+* MongoDB: NoSQL database for storing book data.
+* Express.js: Server-side framework for building the RESTful API.
+* React: Frontend library for building user interfaces.
+* Node.js: JavaScript runtime for running server-side code.
+* Tailwind CSS: Utility-first CSS framework for styling.
+
+## API Endpoints
+
+* GET /books: Get a list of all books.
+* GET /books/:id: Get details of a specific book.
+* POST /books: Add a new book.
+* PUT /books/:id: Update details of a book.
+* DELETE /books/:id: Delete a book.
--- front-end/src/pages/ShowBook.jsx
+++ front-end/src/pages/ShowBook.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+
 
 const ShowBook = () => {
+  const { id } = useParams();
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const url = `http://localhost:3001/books/${id}`;
+  
+
+  useEffect(() => {
+    const findBook = async () => {
+      try 
+      {
+        const res = await fetch(url);
+        if (!res.ok) 
+        {
+          throw new Error('Error while finding book...');
+        }
+        const bookData = await res.json();
+        setBook(bookData);
+      } 
+      catch (error) 
+      {
+        console.log('Error: ', error.message);
+      } 
+      finally 
+      {
+        setLoading(false); 
+      }
+    };
+    findBook();
+  }, [id, url]);
+
+  if (loading) 
+  {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>ShowBook</div>
-  )
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-6">Book Detail</h1> 
+          <div className="max-w-md bg-white p-8 rounded shadow-md">
+              <h1 className="text-3xl font-bold mb-4">{book.title}</h1>
+              <p className="text-gray-600 mb-4">{book.author}</p>
+              <p className="text-gray-600">Published Year: {book.publishedYear}</p>
+          </div>
+      </div>
+    </>
+  );
 }
 
 export default ShowBook;
